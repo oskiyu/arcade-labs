ea6010bb924b9b6adcddad3d98d9da881896267a
oskiyu <oskiyu@gmail.com>
2020-02-27 17:42:32 +0100

    animacion to wapa xd

--- lab02-draw/lab02-draw.py
+++ lab02-draw/lab02-draw.py
@@ -6,11 +6,32 @@ Dibujo to wapo xd
 
 import arcade
 
-arcade.open_window(800, 600, "XD")
+x = 0
+y = 0
+izquierda = True
+arriba = True
 
-arcade.set_background_color(arcade.color.FRESH_AIR)
+def on_draw(delta_time):
+    #Update
+    if on_draw.izquierda:
+        on_draw.x += 5 * 30 * delta_time
+        if (on_draw.x > 800 - 25):
+            on_draw.izquierda = False
+    else:
+        on_draw.x -= 5 * 30 * delta_time
+        if (on_draw.x < 0 + 25):
+            on_draw.izquierda = True
 
-def draw(x, y):
+    if (on_draw.arriba):
+        on_draw.y += 5 * 30 * delta_time
+        if (on_draw.y > 600 - 25):
+            on_draw.arriba = False
+    else:
+        on_draw.y -= 5 * 30 * delta_time
+        if (on_draw.y < 0 + 25):
+            on_draw.arriba = True
+
+    #draw
     arcade.start_render()
 
     arcade.draw_circle_filled(800, 600, 120, arcade.color.YELLOW)
@@ -31,7 +52,7 @@ def draw(x, y):
     arcade.draw_lrtb_rectangle_filled(0, 800, 80, 0, arcade.color.GREEN_YELLOW)
     arcade.draw_rectangle_filled(100, 100, 50, 50, arcade.color.RED)
     arcade.draw_rectangle_filled(100, 100, 50, 20, arcade.color.YELLOW)
-    arcade.draw_text("XD", 300, 200, arcade.color.AMETHYST, 30)
+    arcade.draw_text("XD " + str(int(1 / delta_time)), 300, 200, arcade.color.AMETHYST, 30)
 
     arcade.draw_triangle_filled(250 + 40, 350, 250, 350 - 100, 250 + 80, 350 - 100, arcade  .color.GO_GREEN)
     arcade.draw_triangle_outline(250 + 40, 350, 250, 350 - 100, 250 + 80, 350 - 100, arcade.color.GREEN)
@@ -40,37 +61,23 @@ def draw(x, y):
     arcade.draw_circle_filled(290, 285, 10, arcade.color.WHITE)
     arcade.draw_circle_filled(290, 285, 5, arcade.color.BLACK)
 
-    arcade.draw_circle_filled(x, y, 25, arcade.color.BLUE)
-
     arcade.draw_arc_filled(750, 575, 15, 20, arcade.color.BLACK, 180, 360)
 
-    arcade.finish_render()
+    arcade.draw_circle_filled(on_draw.x, on_draw.y, 25, arcade.color.BLUE)
 
-x = 0
-y = 0
 
-izquierda = True
-arriba = True
+on_draw.x = x
+on_draw.y = y
+on_draw.arriba = arriba
+on_draw.izquierda = izquierda
 
-while (True):
-    if (izquierda):
-        x += 5
-        if (x > 800 - 25):
-            izquierda = False
-    else:
-        x -= 5
-        if (x < 0 + 25):
-            izquierda = True
 
-    if (arriba):
-        y += 5
-        if (y > 600 - 25):
-            arriba = False
-    else:
-        y -= 5
-        if (y < 0 + 25):
-            arriba = True
+def main():
+    arcade.open_window(800, 600, "XD")
+    arcade.set_background_color(arcade.color.FRESH_AIR)
 
-    draw(x, y)
+    arcade.schedule(on_draw, 1/60)
+    arcade.run()
 
-arcade.run()
+
+main()
